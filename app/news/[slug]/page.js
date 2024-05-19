380b2607fab1da40b5fe1bd7333a991c201601a9
Max Hrytsiuk <maxeemja.inc@gmail.com>
2024-05-20 00:47:43 +0300

    archive page added, catch all, parallel routes

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,7 +1,12 @@
 import { DUMMY_NEWS } from '@/dummy-news';
+import { notFound } from 'next/navigation';
 
 export default function NewsDetailsPage({ params }) {
 	const newsItem = DUMMY_NEWS.find((item) => item.slug === params.slug);
+
+	if (!newsItem) {
+		notFound();
+	}
 
 	return (
 		<article className='news-article'>
